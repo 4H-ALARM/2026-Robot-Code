7b60c1a647bfd5ebb0b0ba8547353d8c0dcf229a
Justin Schveighoffer <bajustiba@gmail.com>
2026-01-25 12:30:19 -0500

    Added a climer constants file and plugged those into the ClimberIOKraken

--- src/main/java/frc/robot/subsystems/climber/ClimberIOKraken.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOKraken.java
@@ -7,6 +7,7 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.subsystems.climber.ClimberIO;
+import frc.lib.Constants.ClimberConstants;
 
 /** Add your docs here. */
 public class ClimberIOKraken implements ClimberIO{
@@ -14,14 +15,16 @@ public class ClimberIOKraken implements ClimberIO{
     private  TalonFX m_motor;
     private Slot0Configs m_pIDConfigs;
     private PositionVoltage m_requestedVoltage;
+    private ClimberConstants m_constants;
+
     public ClimberIOKraken() {
-    m_motor = new TalonFX(-1); 
-    m_pIDConfigs = new Slot0Configs()
-     .withKP(1)
-     .withKI(0)
-     .withKD(0);
-    m_requestedVoltage = new PositionVoltage(0).withSlot(0);
-    m_motor.getConfigurator().apply(m_pIDConfigs);
+        m_motor = new TalonFX(-1); 
+        m_pIDConfigs = new Slot0Configs()
+        .withKP(m_constants.climberkp)
+        .withKI(m_constants.climberki)
+        .withKD(m_constants.climberkd);
+        m_requestedVoltage = new PositionVoltage(0).withSlot(0);
+        m_motor.getConfigurator().apply(m_pIDConfigs);
     }
 
     public void moveArm(double speed){
@@ -37,10 +40,9 @@ public class ClimberIOKraken implements ClimberIO{
     public void setArmPosition(double positionInches, ClimberIOInputs imputs){
         double targetPositionInches = positionInches - imputs.currentLengthInches;
 
-        //need to figure out how many inches the arm moves with one rotation
-        double targetPositionRotations;
+        double targetPositionRotations = targetPositionInches/ m_constants.climberRotationToInches;
 
-       // m_motor.setControl(m_requestedVoltage.withPosition(targetPositionRotations));
+        m_motor.setControl(m_requestedVoltage.withPosition(targetPositionRotations));
     }
 
     public void updateInputs(ClimberIOInputs inputs) {
