040a07393569c90b598abd631dc99f7624088765
VM-fll <80290089+VM-fll@users.noreply.github.com>
2026-02-16 18:18:59 -0500

    first iteration for indexer+shooter

---
+++ src/main/java/frc/robot/subsystems/endeffector/ShooterIO.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.lib.enums.TargetEnum;
+import org.littletonrobotics.junction.AutoLog;
+
+/** Add your docs here. */
+public interface ShooterIO {
+  @AutoLog
+  public class ShooterIOInputs {
+    public Rotation2d hoodAngleDegrees;
+    public double shooterVelocityRPM;
+    public boolean shooterMotorIsConnected;
+    public boolean hoodMotorIsConnected;
+    TargetEnum target;
+  }
+
+  public default Rotation2d getHoodAngle() {
+    return new Rotation2d();
+  }
+  ;
+
+  public void changeTarget(TargetEnum target);
+
+  public void updateTuningValues();
+
+  public default void updateInputs(ShooterIOInputs inputs) {}
+}
