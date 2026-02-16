476c2bec5b8ff723c599ee7608e942affa3c7d5c
VM-fll <80290089+VM-fll@users.noreply.github.com>
2026-02-16 18:28:55 -0500

    fixed build time error

--- src/main/java/frc/robot/subsystems/endeffector/IndexerIOKraken.java
+++ src/main/java/frc/robot/subsystems/endeffector/IndexerIOKraken.java
@@ -2,92 +2,33 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.Intake;
-import org.opencv.core.Mat;
+package frc.robot.subsystems.endeffector;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
-
-import frc.lib.Constants.IntakeConstants;
-import frc.robot.subsystems.Intake.IntakeIO;
+import frc.lib.Constants.ShooterConstants;
 
 /** Add your docs here. */
-public class IntakeIOKraken implements IntakeIO {
+public class IndexerIOKraken implements IndexerIO {
 
-TalonFX m_rotationMotor;
-TalonFX m_rotationMotorFollow;
-TalonFX m_intakingMotor;
+  TalonFX m_rotationMotor;
 
-Slot0Configs m_pIDConfigs;
-PositionVoltage m_requestedVoltage;
+  ShooterConstants m_constants;
 
-IntakeConstants m_constants;
+  public IndexerIOKraken() {
+    m_rotationMotor = new TalonFX(m_constants.indexerMotorID);
+  }
 
-
-public IntakeIOKraken() {
-    m_intakingMotor = new TalonFX(m_constants.intakingMotorID);
-    m_rotationMotor = new TalonFX(m_constants.rotationMotorID);
-    m_rotationMotorFollow = new TalonFX(m_constants.rotationMotorFollowID);
-    m_rotationMotorFollow.setControl(new Follower(m_constants.rotationMotorID, MotorAlignmentValue.Opposed));
-    m_pIDConfigs = new Slot0Configs()
-     .withKP(m_constants.angleMotorkp)
-     .withKI(m_constants.angleMotorki)
-     .withKD(m_constants.angleMotorkd);
-    m_rotationMotor.getConfigurator().apply(m_pIDConfigs);
-    m_requestedVoltage = new PositionVoltage(0).withSlot(0);
-
-}
-
-public void resetEncoder() {
-    m_rotationMotor.setPosition(0);
-}
-
-public void changeAngleTest(double speed) {
-    if(speed > 1){
-        speed = 1;
-    }
-    if(speed < -1){
-        speed = -1;
-    }
+  public void setIndexerSpeed(double speed) {
     m_rotationMotor.set(speed);
-}
+  }
 
-public void setAngle(double angleDegrees, IntakeIOInputs inputs){
-    // still need to find the angle before this will work;
-    double motorRotations = (angleDegrees-inputs.rotationDegrees) * m_constants.rotationGearRatio / 360;
+  public void stopIndexer() {
 
-    m_rotationMotor.setControl(m_requestedVoltage.withPosition(motorRotations));
-}
+    m_rotationMotor.set(0);
+  }
 
-public void setIntakeSpeed(double speed) {
-    if(speed > 1){
-        speed = 1;
-    }
-    if(speed < -1){
-        speed = -1;
-    }
-    m_intakingMotor.set(speed);
-
-}
-
-public void stopIntake() {
-
-    m_intakingMotor.set(0);
-
-}
-
-public void updateInputs(IntakeIOInputs inputs) {
-
-    inputs.rotationMotorConnected = m_rotationMotor.isConnected();
-    inputs.intakingMotorConnected = m_intakingMotor.isConnected();
-    inputs.rotationMotorFollowerConnected = m_rotationMotorFollow.isConnected();
+  public void updateInputs(IndexerIOInputs inputs) {
 
     // need to figure out how many encoder units equals one full rotation
-}
-
-
-
+  }
 }
--- src/main/java/frc/robot/subsystems/endeffector/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/endeffector/ShooterIO.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.shooter;
+package frc.robot.subsystems.endeffector;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.lib.enums.TargetEnum;
@@ -29,4 +29,6 @@ public interface ShooterIO {
   public void updateTuningValues();
 
   public default void updateInputs(ShooterIOInputs inputs) {}
+
+  public void setShooterSpeed(double speed);
 }
