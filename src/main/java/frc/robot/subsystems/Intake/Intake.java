078cb9475ccc1f1f46df696e0cbfce875f2bd38a
Isaac Sokolic <isokolicm@gmail.com>
2026-03-02 20:12:50 -0500

    We deleted some more of the alpha subsystem references

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -2,10 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.Intake;
+package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.Intake.IntakeIO.IntakeIOInputs;
+import frc.robot.subsystems.intake.IntakeIO.IntakeIOInputs;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
--- src/main/java/frc/robot/subsystems/endeffector/Shooter.java
+++ src/main/java/frc/robot/subsystems/endeffector/Shooter.java
@@ -4,29 +4,20 @@
 
 package frc.robot.subsystems.endeffector;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drive.Drive;
 
 public class Shooter extends SubsystemBase {
 
-  private TurretIOInputsAutoLogged turretInputs;
-  private TurretIO turret;
   private ShooterIO shooter;
   private Drive drive;
   private IndexerIO indexer;
-  private SpindexerIO m_spindexer;
 
   /** FIX DO NOT WANT TO IMPORT A WHOLE DRIVE */
-  public Shooter(
-      ShooterIO shooter, TurretIO turret, Drive drive, IndexerIO indexer, SpindexerIO spindexer) {
-    this.turretInputs = new TurretIOInputsAutoLogged();
-    this.turret = turret;
+  public Shooter(ShooterIO shooter, Drive drive, IndexerIO indexer) {
     this.shooter = shooter;
     this.drive = drive;
     this.indexer = indexer;
-    this.m_spindexer = spindexer;
   }
 
   @Override
@@ -35,19 +26,11 @@ public class Shooter extends SubsystemBase {
     // this.turret.turretPeriodic(drive.getPose());
   }
 
-  public Rotation2d getTargetTurretAngle(Pose2d pose) {
-    return turret.getTargetTurretAngle(pose);
-  }
-
   public void spinShooter(double speed) {
     shooter.setShooterSpeed(speed);
   }
 
   public void setIndexerSpeed(double indexerSpeed) {
     indexer.setIndexerSpeed(indexerSpeed);
-  }
-
-  public void setSpindexerSpeed(double speed) {
-    m_spindexer.setSpindexerSpeed(speed);
   }
 }
