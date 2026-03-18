72227b8f36b68d8132c61d3fee44e27972dcbcb7
Justin Schveighoffer <bajustiba@gmail.com>
2026-03-18 18:46:17 -0400

    finished deployIntake command

---
+++ src/main/java/frc/robot/commands/DeployIntake.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.lib.Constants.IntakeConstants;
+import frc.robot.subsystems.intake.Intake;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class DeployIntake extends Command {
+  Intake m_intake;
+  boolean m_isIntakeUp;
+  /** Creates a new DeployIntake. */
+  public DeployIntake(Intake intake) {
+    this.m_intake = intake;
+    this.m_isIntakeUp = intake.getAngle() < IntakeConstants.rotationDownDegrees/4;
+    addRequirements(intake);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (m_isIntakeUp) {
+      m_intake.setRotationDown();
+    }
+    else {
+      m_intake.setRotationUp();
+    }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
